af75de6e961a1cb96bfbd3e361f0da84b968f236
yoongsunglee <acegauss@naver.com>
2022-01-17 23:35:02 +0900

    8.three_sum

---
+++ linear_datastructure/answer/8.three_sum.py
@@ -0,0 +1,35 @@
+#배열을 입력받아 합으로 0을 만들 수 있는 3개의 엘리먼트를 출력하시오.
+from typing import List
+
+def threeSum(nums : List[int]) -> List[List[int]]:
+    results = []
+    nums.sort()
+
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            sum = nums[i] + nums[left] + nums[right]
+            if sum < 0:
+                left += 1
+            elif sum > 0:
+                right -= 1
+            else:
+                results.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+
+    return results
+
+
+if __name__ == '__main__':
+    nums = [-1,0,1,2,-1,-4]
+    print(threeSum(nums))
---
+++ linear_datastructure/assignment/8.three_sum.py
@@ -0,0 +1,11 @@
+#배열을 입력받아 합으로 0을 만들 수 있는 3개의 엘리먼트를 출력하시오.
+from typing import List
+
+def threeSum(nums : List[int]) -> List[List[int]]:
+    results = []
+    return results
+
+
+if __name__ == '__main__':
+    nums = [-1,0,1,2,-1,-4]
+    print(threeSum(nums))
