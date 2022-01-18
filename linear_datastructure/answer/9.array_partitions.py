e485ed629a11565e2de63c764894ce79949bb47d
yoongsunglee <acegauss@naver.com>
2022-01-18 23:45:48 +0900

    array_partitions

---
+++ linear_datastructure/answer/9.array_partitions.py
@@ -0,0 +1,10 @@
+#n개의 페어를 이용한 min(a,b)의 합으로 만들 수 있는 가장 큰 수를 출력하라.
+from typing import List
+
+def arrayPartition(nums : List[int]) -> int:
+    return sum(sorted(nums)[::2])
+
+
+if __name__ == '__main__':
+    nums = [1,4,3,2]
+    print(arrayPartition(nums)) #4
